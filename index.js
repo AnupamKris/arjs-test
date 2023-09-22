4e3290b91c35274e2207b4e9c325aab59b9a4fb3
Anupam Krishna <anupamkris13262@gmail.com>
2023-09-22 12:07:39 +0530

    fix

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-const canvas = document.getElementById('canvas1');
+import * as THREE from 'three';
+import * as THREEx from './node_modules/@ar-js-org/ar.js/three.js/build/ar-threex-location-only.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+function main() {
+    const canvas = document.getElementById('canvas1');
 
-const arjs = new THREEx.LocationBased(scene, camera);
-const cam = new THREEx.WebcamRenderer(renderer);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, 1.33, 0.1, 10000);
+    const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
-const geom = new THREE.BoxGeometry(20, 20, 20);
-const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const box = new THREE.Mesh(geom, mtl);
+    const arjs = new THREEx.LocationBased(scene, camera);
+    const cam = new THREEx.WebcamRenderer(renderer);
 
-// Create the device orientation tracker
-const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
+    const geom = new THREE.BoxGeometry(20, 20, 20);
+    const mtl = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const box = new THREE.Mesh(geom, mtl);
 
-// Change this to a location close to you (e.g. 0.001 degrees of latitude north of you)
-arjs.add(box, -0.72, 51.051);
+    // Create the device orientation tracker
+    const deviceOrientationControls = new THREEx.DeviceOrientationControls(camera);
 
-arjs.startGps();
+    // Change this to a location close to you (e.g. 0.001 degrees of latitude north of you)
+    arjs.add(box, -0.72, 51.051);
 
-requestAnimationFrame(render);
+    arjs.startGps();
 
-function render() {
-    if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
-        renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
-        const aspect = canvas.clientWidth / canvas.clientHeight;
-        camera.aspect = aspect;
-        camera.updateProjectionMatrix();
-    }
-
-    // Update the scene using the latest sensor readings
-    deviceOrientationControls.update();
-
-    cam.update();
-    renderer.render(scene, camera);
     requestAnimationFrame(render);
+
+    function render() {
+        if (canvas.width != canvas.clientWidth || canvas.height != canvas.clientHeight) {
+            renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
+            const aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.aspect = aspect;
+            camera.updateProjectionMatrix();
+        }
+
+        // Update the scene using the latest sensor readings
+        deviceOrientationControls.update();
+
+        cam.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
 }
+
+main();
